068fb0094174f7b919acd0f5de1eb2cc5bdfd937
Jzavier Timm <jztimm@gmail.com>
2020-06-21 18:33:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
-    puts "FIZZBUZZ"
+    "FIZZBUZZ"
   elsif number % 3 == 0 && number % 5 != 0
-    puts "Fizz"
+    "Fizz"
   elsif number % 5 == 0 && number % 3 != 0
-    puts "Buzz"
+    "Buzz"
   end  
 end
